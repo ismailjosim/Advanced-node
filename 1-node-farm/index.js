77c6e7329c68fc65899c20fe125b1eec4545a983
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-03-06 23:25:59 +0600

    Create NODE FARM app with Node application

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -28,8 +28,30 @@ console.log("Welcome to the Node Concept!");
 */
 //==========> SERVER
 
-const data = fs.readFileSync(`./dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+
+}
+
+
+const tempOverview = fs.readFileSync(`${ __dirname }/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${ __dirname }/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${ __dirname }/templates/template-product.html`, 'utf-8');
+
+// Read Product Data from data.json file.
+const productData = fs.readFileSync(`${ __dirname }/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(productData)
 
 
 
@@ -37,22 +59,26 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-
+    // about page
     if (pathName === '/about') {
         res.end("You're Now in about Route")
     }
+    // overview page
+    else if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        const cardHTMl = dataObj.map(item => replaceTemplate(tempCard, item)).join('');
 
-    else if (pathName === '/' || pathName === '/home') {
-        res.end("You're Now in Home Route")
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHTMl)
+        res.end(output);
     }
 
-    // Read file data from filesystem
+    // API page
     else if (pathName === '/api') {
         res.writeHead(200, { 'content-type': 'application/json' })
-        res.send(dataObj)
+        res.end(dataObj)
     }
 
-
+    // 404 page
     else {
         res.writeHead(404, {
             'content-type': 'text/html'
