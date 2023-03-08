d2ba2c68e5cc8e9d57fa25757b58e39cc1aa790f
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-03-08 15:30:42 +0600

    Get & post Tours data

--- natours/app.js
+++ natours/app.js
@@ -1,25 +1,69 @@
 const express = require('express');
+const fs = require('fs');
 
 
 const app = express();
 const port = 5000;
 
+// Middleware
+app.use(express.json());
+
 
 //==> Default end point
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'Natours server Connected 🎉🎉'
-    });
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({
+//         success: true,
+//         message: 'Natours server Connected 🎉🎉'
+//     });
+// });
 
-//===> Get All reviews
-app.post('/', (req, res) => {
-    res.send("You can post to this input");
+
+// app.post('/', (req, res) => {
+//     res.send("You can post to this input");
+// })
+
+
+//===> Get All tours
+const tours = JSON.parse(fs.readFileSync(`${ __dirname }/dev-data/data/tours-simple.json`));
+app.get('/api/v1/tours', (req, res) => {
+    try {
+        res.status(200).json({
+            status: "success",
+            results: tours.length,
+            data: {
+                tours: tours
+            }
+        })
+    } catch (error) {
+        res.json({
+            status: "fail",
+            message: error.message
+        })
+    }
 })
 
+//===> Post new tour
+app.post('/api/v1/tours', (req, res) => {
+    try {
+        const newId = tours[tours.length - 1].id + 1;
+        const newTour = Object.assign({ id: newId }, req.body);
 
-
+        tours.push(newTour);
+        fs.writeFile(`${ __dirname }/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    tour: newTour
+                }
+            })
+        });
+    } catch (error) {
+        res.json({
+            status: "fail",
+            message: error.message
+        })
+    }
+})
 
 
 app.listen(port, () => {
