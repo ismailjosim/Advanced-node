d684c1681dd3f412ba3a8e231ebb0456aec7dff5
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-03-09 23:23:44 +0600

    kKsection completed

--- natours/app.js
+++ natours/app.js
@@ -6,19 +6,21 @@ const toursRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 const reviewsRouter = require('./routes/reviewRoutes');
 
-
 const app = express();
-
 
 // 1): MIDDLEWARE
 app.use(express.json());
-app.use(morgan('dev'));
 
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
+
+// to use files from filesystem: use the following middleware
+app.use(express.static(`${ __dirname }/public`));
 
 // routes
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/reviews', reviewsRouter);
-
 
 module.exports = app;
--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,10 @@
-const app = require('./app')
+const dotenv = require('dotenv');
 
-const port = 5000;
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Natours server running on Port: ${ port }`);
